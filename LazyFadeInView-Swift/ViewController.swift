8ccc4c744465022203be05dcefc24bc43d56013e
tuyou <yoututouch@gmail.com>
2016-02-25 20:15:16 +0800

    Add toggle text

--- LazyFadeInView-Swift/ViewController.swift
+++ LazyFadeInView-Swift/ViewController.swift
@@ -14,9 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let lazyFadeInView: LazyFadeInView? = LazyFadeInView.init(frame: CGRectMake(20, 100, UIScreen.mainScreen().bounds.size.width - 40, 200))
-        lazyFadeInView?.textColor = UIColor.blackColor()
-        lazyFadeInView?.text = "Stray birds of summer come to my window to sing and fly away. And yellow leaves of autumn, which have no songs, flutter and fall there with a sign. O Troupe of little vagrants of the world, leave your footprints in my words.";
+        self.lazyFadeInView = LazyFadeInView.init(frame: CGRectMake(20, 100, UIScreen.mainScreen().bounds.size.width - 40, 200))
+        self.lazyFadeInView?.textColor = UIColor.blackColor()
+        self.lazyFadeInView?.text = strayBirds
         self.view.addSubview(lazyFadeInView!)
     }
 
@@ -24,7 +24,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func textToggled(sender: AnyObject) {
+        self.lazyFadeInView?.text = (sender as! UISwitch).on ? strayBirds : qianChiBiFu
+    }
+    var testView: UIView?
+    var lazyFadeInView: LazyFadeInView!
+    let strayBirds = "Stray birds of summer come to my window to sing and fly away. And yellow leaves of autumn, which have no songs, flutter and fall there with a sign. O Troupe of little vagrants of the world, leave your footprints in my words."
+    let qianChiBiFu = "惟江上之清风，与山间之明月，耳得之而为声，目遇之而成色。取之无禁，用之不竭。是造物者之无尽藏也，而吾与子之所共适。"
 }
 

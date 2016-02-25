3e810009bb4275550fd69a8021fca26797a512ec
tuyou <yoututouch@gmail.com>
2016-02-25 20:17:31 +0800

    minor changes

--- LazyFadeInView-Swift/ViewController.swift
+++ LazyFadeInView-Swift/ViewController.swift
@@ -28,8 +28,8 @@ class ViewController: UIViewController {
     @IBAction func textToggled(sender: AnyObject) {
         self.lazyFadeInView?.text = (sender as! UISwitch).on ? strayBirds : qianChiBiFu
     }
-    var testView: UIView?
-    var lazyFadeInView: LazyFadeInView!
+    
+    var lazyFadeInView: LazyFadeInView?
     let strayBirds = "Stray birds of summer come to my window to sing and fly away. And yellow leaves of autumn, which have no songs, flutter and fall there with a sign. O Troupe of little vagrants of the world, leave your footprints in my words."
     let qianChiBiFu = "惟江上之清风，与山间之明月，耳得之而为声，目遇之而成色。取之无禁，用之不竭。是造物者之无尽藏也，而吾与子之所共适。"
 }
